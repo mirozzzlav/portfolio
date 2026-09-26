9b3cf196161abc47e8997d4a2f4aa42bf3e2e060
miro <miro.furinda@proton.me>
2026-09-26 16:10:14 +0200

    dev server access via net

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   preview: {
+    host: "0.0.0.0",
     port: 8080,
     strictPort: true
   },
   server: {
+    host: "0.0.0.0",
     port: 8080,
     proxy: {
       "/api": "http://127.0.0.1:8001"
